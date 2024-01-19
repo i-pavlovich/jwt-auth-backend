7c5eba10d0dc51f7996bb5966c12aa10ef9d311b
Ivan Pavlovich <ivan.pavlovich.business@gmail.com>
2024-01-19 13:35:21 +0300

    chore: edit run method

--- src/main.py
+++ src/main.py
@@ -2,21 +2,13 @@ import uvicorn
 from fastapi import FastAPI
 
 
-def create_app() -> FastAPI:
-    app = FastAPI()
+app = FastAPI()
 
-    @app.get("/healthcheck")
-    def healthcheck() -> dict[str, str]:
-        return {"status": "ok"}
 
-    return app
+@app.get("/healthcheck")
+def healthcheck() -> dict[str, str]:
+    return {"status": "ok"}
 
 
 if __name__ == "__main__":
-    config = uvicorn.Config(
-        "main:create_app",
-        reload=True,
-        factory=True,
-    )
-    server = uvicorn.Server(config)
-    server.run()
+    uvicorn.run("main:app", reload=True)
